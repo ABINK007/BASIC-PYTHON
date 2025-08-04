08c286ed936d54854ecb5a476cdce4d3b09ca689
ABINK007 <abinash.kar.november18@gmail.com>
2025-08-04 20:52:10 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,67 @@
+#wap in python to assign values in different variables and print it......
+
+#Value = variables
+
+Name = "Abinash","Abhisek"     #Here in name we will assign string  
+                               #strings mean multiple character is write in between two commas like(" String ")
+age = 23,24
+price = 5.22,8.99
+price=23
+
+
+print(Name)
+print(age)
+print(price)
+
+
+#let us see how we can assign values in different variables and print it......
+myvar = "Abinash"  #Assigning string value to variable myvar
+myvar1 = 23       #Assigning integer value to variable myvar1
+my_var2 = 5.22     #Assigning float value to variable myvar2
+MYVAR = True         #Assigning boolean value to variable MYVAR(This is different from myvar due to case sensitivity
+_my_var3 = None         #Assigning None value to variable _my_var3 (None represents the absence of a value)(This is different 
+#from myvar due to case sensitivity)
+#But 1var = "Abinash"  #This is not a valid variable name because it starts with a digit...
+my7var= "Abhisekh"  #Assigning string value to variable my7var (This is valid because it starts with a letter)
+
+'''we know That Variable = values
+variable = Memory location......
+Here shows we assign different values in diferent variables and print those values present in variables
+comma is used to separate parameters in a function call, or in the function header....'''
+
+
+'''Rules for variable names in Python:
+1. Variable names can only contain letters (a-z, A-Z), digits (0-9), and underscores (_).
+2. Variable names cannot start with a digit.
+3. Variable names are case-sensitive (e.g., `myVar` and `myvar` are different variables).
+4. Variable names cannot be a reserved keyword in Python (e.g., `if`, `else`, `while`, etc.).
+5. Variable names should be descriptive and meaningful to improve code readability.'''
+
+
+#wap in python to know about python adentifiers....
+
+variable1 = 25
+
+
+#.......................................................................................................................
+#1variable = 32  #Here it shows error due to identifiers is start with digit here....
+#.......................................................................................................................
+
+valid_abinash=23
+
+
+#.......................................................................................................................
+#valid abinash=35  #Here it shows error due to here in identifiers there are blank space in between valid and abinash
+#.......................................................................................................................
+
+
+#.......................................................................................................................
+'''@final = 25
+!start =35'''
+#.......................................................................................................................
+
+
+print(variable1)
+print(valid_abinash)
+print("         abinash         ")   #here we use tab to cheak how whitespace work in python....
+
