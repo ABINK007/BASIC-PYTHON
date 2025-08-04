85f73f84d666110d1ae768bc0b6b64d5ee699c73
ABINK007 <abinash.kar.november18@gmail.com>
2025-08-04 20:52:40 +0530

    Add files via upload

---
+++ Rectangle and square.py
@@ -0,0 +1,17 @@
+#it is also code for to understand variables in python
+
+# Q:-  wap in python to take length and breadth from user and find rectangle's perimeter and area
+# Q:-  wap in python to take length from user and find square's perimeter and area
+
+
+
+# It is to found perimeter and area of a reactangle.....
+length =9
+breadth =4
+print("Perimeter of reactangle is", 2*(length + breadth))
+print("area of reactangle is", (length * breadth))
+
+# It is to found perimeter and area of a square.....
+length = 9
+print("Perimeter of square is", 4 *length)
+print("area of square is", (length * length))
