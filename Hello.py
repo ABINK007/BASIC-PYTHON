079678731782e539a988ac235859f5d69eab06dc
ABINK007 <abinash.kar.november18@gmail.com>
2025-08-04 19:37:50 +0530

    Add files via upload
    WRITE HELLOWORLD AND HOW TO WRITE COMMENT IN A PYTHON

---
+++ Hello.py
@@ -0,0 +1,11 @@
+# (Character (#) star show one line comment in python)
+'''And (character ('''''') is show multiple line comment in python)'''
+
+'''It is is my 1st program in python
+And here i am print the question like
+
+(WAP in Python to print hello world in computer..... )'''
+
+print("Hello world")
+
+'''print(Abinash)'''#Here it shows it is comment line in python....
