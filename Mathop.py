a8e061e5a2705089369f2e1bbcda78ee1a8e2093
ABINK007 <abinash.kar.november18@gmail.com>
2025-08-04 20:50:58 +0530

    Add files via upload

---
+++ Mathop.py
@@ -0,0 +1,17 @@
+#wap in python to dispaly mathmatics operation like (sum, difference, multiplication, divisionl ,and modulo) of two given number....
+
+N1=1000  #Take one number
+N2=500   #Take secoend number
+
+sum=N1+N2   #for logic sum
+diff=N1-N2
+mul=N1*N2
+div=N1/N2
+mod=N1%N2
+
+print(sum)   #print that sum.....
+print(diff)
+print(mul)
+print(div)
+print(mod)   #It print reminder of two number....  Here 45 modulo 25 is reminder is 20
+#like 45/25 = 1 and reminder 20..... 
